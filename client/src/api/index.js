d12b0e7f81949b5cf5f45627a21040a2c47fdc27
surajshukla <surajshukla5604@gmail.com>
2023-07-25 19:58:28 +0530

    connected backend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const DEVELOPMENT_URL = 'http://localhost:5000';
-const DEPLOYMENT_URL = '';
+const DEPLOYMENT_URL = 'https://youtubeclone-backend-u64g.onrender.com/';
 
-const API = axios.create( {baseURL: DEVELOPMENT_URL} );
+const API = axios.create( {baseURL: DEPLOYMENT_URL} );
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('userProfile')) {
