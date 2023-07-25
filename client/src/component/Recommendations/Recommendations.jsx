591537cbdb0ed9aa0ffb50641dfc06861c0e75da
surajshukla <surajshukla5604@gmail.com>
2023-07-25 19:16:16 +0530

    fixed recommendation responsiveness

--- client/src/component/Recommendations/Recommendations.jsx
+++ client/src/component/Recommendations/Recommendations.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const RecommendationButton = ({recommendation}) => {
   return (
-    <div className='rounded-lg bg-[#ffffff1b] py-2 px-4'>
+    <div className='rounded-lg bg-[#ffffff1b] py-2 px-4 '>
       <h1 className='text-xl font-medium text-white'>{recommendation}</h1>
     </div>
   );
@@ -10,7 +10,7 @@ const RecommendationButton = ({recommendation}) => {
 const Recommendations = () => {
   const recommendations = [ 1, 2, 3, 4, 5, 6];
   return (
-    <div className='flex m-12 gap-4'>
+    <div className='flex m-12 gap-4 sm:overflow-hidden overflow-x-scroll'>
       <RecommendationButton recommendation='All' />
       {recommendations.map((recommendation) => <RecommendationButton recommendation='Recommendation' />)}
     </div>
